bd0bd6d0de38edbeabcf25d656ff945405f6f77d
zestsystem <mikeyim@unitetheculture.org>
2023-09-10 20:18:29 -0700

    set up dev env with nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Get Turso working with Cloudflare Worker";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      corepackEnable = pkgs.runCommand "corepack-enable" {} ''
+        mkdir -p $out/bin
+        ${pkgs.nodejs-18_x}/bin/corepack enable --install-directory $out/bin
+      '';
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs-18_x
+            just
+            zsh
+            sqld
+            turso-cli
+            corepackEnable
+          ];
+          shellHook = ''
+            zsh
+
+          '';
+        };
+      };
+    });
+}
